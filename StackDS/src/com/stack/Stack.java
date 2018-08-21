44583f94737cb4cb7785f5ba20c009363ad70d2e
Naveen Kumaravelmurugaian <oknaveen9395@gmail.com>
2018-08-21 14:11:27 +0530

    pop method implemented

--- StackDS/src/com/stack/Stack.java
+++ StackDS/src/com/stack/Stack.java
@@ -4,34 +4,45 @@
 package com.stack;
 
 public class Stack {
-private int [] stack;
-private int top;
-private int size;
+	private int[] stack;
+	private int top;
+	private int size;
 
-public Stack() {
-	stack = new int[50];
-	top = -1;
-	size = 50;
-}
-public Stack(int size) {
-	top = -1;
-	this.size = size;
-	stack = new int[this.size];
-}
-public boolean push(int number){
-	if(!isFull()==true){
-	top++;
-	stack[top]=number;
-	return true;
+	public Stack() {
+		stack = new int[50];
+		top = -1;
+		size = 50;
 	}
-	else {
-		return false;
-	}
-}
-public boolean isFull(){
-	return (top==stack.length-1);
-	
-	
-}
-}
 
+	public Stack(int size) {
+		top = -1;
+		this.size = size;
+		stack = new int[this.size];
+	}
+
+	public boolean push(int number) {
+		if (!isFull()) {
+			top++;
+			stack[top] = number;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean isFull() {
+		return (top == stack.length - 1);
+
+	}
+
+	public int pop() {
+
+		top--;
+		return stack[top];
+
+	}
+
+	public boolean isempty() {
+		return (top == -1);
+	}
+}
